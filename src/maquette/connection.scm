c17581e28d15e69228914458fd216366f010235f
ktakashi <ktakashi@ymail.com>
2016-02-25 15:19:10 +0100

    Adding connection and connection pool

---
+++ src/maquette/connection.scm
@@ -0,0 +1,198 @@
+;;; -*- mode:scheme; coding:utf-8; -*-
+;;;
+;;; maquette/connection.scm - Connection and connection pool
+;;;  
+;;;   Copyright (c) 2016  Takashi Kato  <ktakashi@ymail.com>
+;;;   
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
+;;;   
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;  
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;  
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;  
+
+(library (maquette connection)
+    (export make-maquette-connection maquette-connection?
+	    maquette-connection-dbi-connection
+	    maquette-connection-connect!
+	    maquette-connection-open?
+	    maquette-connection-close!
+	    maquette-connection-reconnect!
+	    open-maquette-connection
+	    maquette-connection-start-transaction!
+	    maquette-connection-commit!
+	    maquette-connection-rollback!
+
+	    ;; connection pool
+	    make-maquette-connection-pool maquette-connection-pool?
+	    maquette-connection-pool-get-connection
+	    maquette-connection-pool-return-connection
+	    maquette-connection-pool-release!
+	    maquette-connection-pool-re-pool!
+	    maquette-connection-pool-connection-available?
+	    maquette-connection-pool-available-connection-count
+
+	    ;; transaction
+	    with-maquette-transaction
+	    call-with-available-connection
+	    )
+    (import (rnrs)
+	    (clos user)
+	    (sagittarius object)
+	    (util concurrent)
+	    (srfi :18)
+	    (srfi :26)
+	    (dbi))
+
+(define-class <maquette-connection> ()
+  ((dbi-connection :init-keyword :dbi-connection :init-value #f
+		   :reader maquette-connection-dbi-connection)
+   (dsn :init-keyword :dsn)
+   (options :init-keyword :options)))
+(define (maquette-connection? o) (is-a? o <maquette-connection>))
+(define (make-maquette-connection dsn . opt) 
+  (make <maquette-connection> :dsn dsn :options opt))
+(define (maquette-connection-open? conn)
+  (is-a? (maquette-connection-dbi-connection conn) <dbi-connection>))
+
+(define (maquette-connection-connect! conn)
+  (unless (maquette-connection-open? conn)
+    (set! (~ conn 'dbi-connection)
+	  ;; it's better to have :auto-commit option enabled
+	  ;; but we can't assume it's supported by DBD.
+	  (apply dbi-connect (~ conn 'dsn) (~ conn 'options))))
+  conn)
+
+(define (maquette-connection-close! conn)
+  (when (~ conn 'dbi-connection)
+    (dbi-close (maquette-connection-dbi-connection conn)))
+  (set! (~ conn 'dbi-connection) #f)
+  conn)
+
+(define (maquette-connection-reconnect! c)
+  (when (~ c 'dbi-connection) (maquette-connection-close! c))
+  (maquette-connection-connect! c))
+
+;; convenient procedure
+(define (open-maquette-connection dsn . opt)
+  (maquette-connection-connect! (apply make-maquette-connection dsn opt)))
+(define (maquette-connection-start-transaction! c)
+  (dbi-execute-using-connection! (~ c 'dbi-connection) "BEGIN")
+  c)
+(define (maquette-connection-commit! c) (dbi-commit! (~ c 'dbi-connection)) c)
+(define (maquette-connection-rollback! c) 
+  (dbi-rollback! (~ c 'dbi-connection)) 
+  c)
+
+;; connection pool
+;; We do very simple connection pooling here. The basic strategy is
+;; that using shared-queue to manage connection retrieval and returning.
+(define-class <maquette-connection-pool> ()
+  ((pool :init-keyword :pool) ;; shared-queue
+   (connections :init-keyword :connections) ;; actual connections
+   (lock :init-form (make-mutex))))
+
+(define (make-maquette-connection-pool max-connection dsn . options)
+  (define (make-connections n)
+    (let loop ((i 0) (r '())) 
+      (if (= i n) 
+	  r
+	  (loop (+ i 1) 
+		(cons (apply open-maquette-connection dsn options) r)))))
+  (let ((sq (make-shared-queue))
+	(conns (make-connections max-connection)))
+    (for-each (cut shared-queue-put! sq <>) conns)
+    (make <maquette-connection-pool> :pool sq :connections conns)))
+
+(define (maquette-connection-pool? o) (is-a? o <maquette-connection-pool>))
+
+(define (maquette-connection-pool-get-connection cp . opt)
+  (let ((r (apply shared-queue-get! (~ cp 'pool) opt)))
+    (if (maquette-connection? r)
+	(maquette-connection-connect! r)
+	r)))
+
+(define (maquette-connection-pool-return-connection cp conn)
+  (unless (memq conn (~ cp 'connections))
+    (assertion-violation 'maquette-connection-pool-return-connection
+			 "not a managed connection" conn))
+  ;; avoid duplicate connection to be pushed
+  ;; so returning is a bit more expensive than retrieving
+  (mutex-lock! (~ cp 'lock))
+  (unless (shared-queue-find (~ cp 'pool) (lambda (e) (eq? e conn)))
+    ;; if the connection is closed, then retriever makes sure the connectivity
+    ;; so simply push
+    (shared-queue-put! (~ cp 'pool) conn))
+  (mutex-unlock! (~ cp 'lock))
+  cp)
+
+(define (maquette-connection-pool-connection-available? cp)
+  (let ((pool (~ cp 'pool)))
+    (and pool (not (shared-queue-empty? pool)))))
+
+(define (maquette-connection-pool-available-connection-count cp)
+  (let ((pool (~ cp 'pool)))
+    (if pool 
+	(shared-queue-size pool)
+	0)))
+
+;; release all connection
+;; TODO should we raise an error if managed connection(s) are used?
+(define (maquette-connection-pool-release! cp)
+  ;; closes all connection
+  (for-each maquette-connection-close! (~ cp 'connections))
+  (set! (~ cp 'pool) #f)
+  cp)
+
+;; Should we provide this?
+(define (maquette-connection-pool-re-pool! cp)
+  (unless (~ cp 'pool) 
+    (let ((sq (make-shared-queue)))
+      (set! (~ cp 'pool) sq)
+      (for-each (cut shared-queue-put! sq <>) (~ cp 'connections))))
+  cp)
+
+;; High level APIs
+;; this would block. it is users' responsibility to check if there's an
+;; available connection.
+(define (call-with-available-connection cp proc)
+  (let ((conn #f))
+    (dynamic-wind
+	(lambda () (set! conn (maquette-connection-pool-get-connection cp)))
+	(lambda () (proc conn))
+	(lambda () (maquette-connection-pool-return-connection cp conn)))))
+
+;; transaction
+(define-syntax with-maquette-transaction
+  (syntax-rules ()
+    ((_ conn expr ...)
+     (let ((c conn))
+       (dynamic-wind
+	   (lambda () 
+	     ;; some DBD throws an error if there's already a transaction.
+	     (guard (e (else #t)) (maquette-connection-start-transaction! c)))
+	   (lambda () expr ...)
+	   (lambda ()
+	     ;; some DBD throws an error if there's no transaction.
+	     (guard (e (else #t)) (maquette-connection-rollback! c))))))))
+	       
+
+
+)
---
+++ tests/connection.scm
@@ -0,0 +1,112 @@
+(import (rnrs)
+	(sagittarius)
+	(maquette connection)
+	(dbi)
+	(clos user)
+	(srfi :117)
+	(srfi :64)
+	(text sql))
+
+(test-begin "Maquette - Connection")
+
+(define db-file "test.db")
+(define dsn (format "dbi:sqlite3:database=~a" db-file))
+
+(test-assert (make-maquette-connection dsn))
+(test-assert (maquette-connection?
+	      (make-maquette-connection dsn)))
+
+(test-assert (make-maquette-connection-pool 1 dsn))
+(test-assert (maquette-connection-pool?
+	      (make-maquette-connection-pool 1 dsn)))
+
+(test-error assertion-violation?
+	    (maquette-connection-pool-return-connection
+	      (make-maquette-connection-pool 1 dsn)
+	      (make-maquette-connection dsn)))
+
+(cond-expand
+ ((library (dbd sqlite3))
+  ;; first drop all
+  (when (file-exists? db-file) (delete-file db-file))
+
+  ;; auto-commit #t would be better for testing
+  (define connection (open-maquette-connection dsn :auto-commit #t))
+  ;; for testing 1 is enough
+  (define pool (make-maquette-connection-pool 1 dsn :auto-commit #t))
+
+  ;; connection
+  (test-assert (maquette-connection-open? connection))
+  (test-assert (maquette-connection? 
+		(maquette-connection-connect! connection)))
+  (let ((old (maquette-connection-dbi-connection connection)))
+    (test-assert
+     (maquette-connection? (maquette-connection-connect! connection)))
+    ;; maquette-connection-connect! doesn't do anything if connection
+    ;; is not closed.
+    (test-equal old
+		(maquette-connection-dbi-connection
+		 (maquette-connection-connect! connection)))
+    (test-assert (maquette-connection? 
+		  (maquette-connection-close! connection)))
+    (test-assert "double closing"
+		 (maquette-connection? 
+		  (maquette-connection-close! connection)))
+    (test-assert (not (maquette-connection-open? connection)))
+    (test-assert (maquette-connection? 
+		  (maquette-connection-reconnect! connection))))
+  (let ((old (maquette-connection-dbi-connection connection)))
+    ;; reconnection actually re-connect the connection
+    ;; so not be the same
+    (test-assert 
+     (not (eq? old
+	       (maquette-connection-dbi-connection 
+		(maquette-connection-reconnect! connection))))))
+
+  (test-assert (maquette-connection-start-transaction! connection))
+  (test-assert (maquette-connection-commit! connection))
+  (test-assert (maquette-connection-start-transaction! connection))
+  (test-assert (maquette-connection-rollback! connection))
+
+  (test-equal 'ok (with-maquette-transaction connection 'ok))
+
+  ;; connection pool
+  (let ((c (maquette-connection-pool-get-connection pool)))
+    (test-assert (maquette-connection? c))
+    (test-assert (maquette-connection-open? c))
+    (test-assert (not (maquette-connection-pool-connection-available? pool)))
+
+    (test-equal 'timeout
+		(maquette-connection-pool-get-connection pool 0 'timeout))
+
+    (maquette-connection-close! c)
+    (maquette-connection-pool-return-connection pool c)
+    (let ((c2 (maquette-connection-pool-get-connection pool)))
+      (test-assert (maquette-connection-open? c))
+      (maquette-connection-pool-return-connection pool c))
+    ;; oops
+    (test-equal 1
+		(maquette-connection-pool-available-connection-count
+		 (maquette-connection-pool-return-connection pool c)))
+
+    (test-assert (maquette-connection-pool?
+		  (maquette-connection-pool-release! pool)))
+    (test-assert (not (maquette-connection-open? c))))
+
+  ;; re-opened
+  (test-assert (maquette-connection-pool-re-pool! pool))
+  (test-assert (maquette-connection-pool-connection-available? pool))
+
+  (test-equal 'ok (call-with-available-connection pool (lambda (conn) 'ok)))
+  
+  (when (file-exists? db-file) (guard (e (else #t))(delete-file db-file)))
+
+  )
+ (else 
+  (display "SKIPPED!" (current-error-port)) (newline (current-error-port))
+  (display "Actual database access tests require SQLite3 binding" 
+	   (current-error-port))
+  (newline (current-error-port))
+  ))
+
+(test-end)
